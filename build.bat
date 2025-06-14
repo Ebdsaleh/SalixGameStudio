81fc1097ab7dfa1b93b7d3dacb046521aa8fb8ce
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-14 20:13:28 +1000

    initial commit

---
+++ build.bat
@@ -0,0 +1,28 @@
+@echo off
+:: build.bat - A simple build script for the Salix Game Studio project
+
+echo --- Checking for build directory
+if not exist build mkdir build
+
+echo --- Starting build...
+:: Command Breakdown:
+:: cl -> The compiler
+:: /EHsc -> Enable C++ Exceptions
+:: /I vendor\SDL2\include -> Tell the compiler where to find SDL's header files
+:: /Fo:build\ -> Tell the compiler where to the object file (main.obj)
+:: /Fe:build\salix.exe -> Tell the compiler what to name the final executable
+:: src\main.cpp -> The source file to compile
+:: /link -> Anything after this a command for the linker
+:: /LIBPATH:vendor\SDL2\lib\x64 -> Tell the linker where to find the .lib files
+:: SDL2.lib SDL2main.lib -> the specific library files to link against
+
+cl /EHsc /I vendor\SDL2\include /Fo:build\ /Fe:build\salix.exe src\main.cpp vendor\SDL2\lib\x64\SDL2.lib vendor\SDL2\lib\x64\SDL2main.lib Shell32.lib /link /SUBSYSTEM:CONSOLE
+
+:: Check if the build was successful before copying the DLL
+if exist build\salix.exe (
+    echo --- Copying DLL...
+    copy vendor\SDL2\lib\x64\SDL2.dll build\
+    echo --- Build Successful.
+) else (
+    echo --- Build FAILED.
+)
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <SDL.h>
+#include <iostream>
+
+
+int main(int argc, char* argv[]) { 
+    std::cout << "Salix Engine: Status... Nominal." << std::endl;
+    return 0;
+}
