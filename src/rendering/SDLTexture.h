a3eb3111ed0b8c45b4cdb2142c985660f09d6559
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-17 02:28:17 +1000

    Created 'SDLTexture.h'.

---
+++ src/rendering/SDLTexture.h
@@ -0,0 +1,24 @@
+// SDLTexture.h
+#pragma once
+#include "ITexture.h"
+#include <SDL.h>
+
+// This is the concrete SDL implementation of our ITexture interface.
+class SDLTexture : public ITexture {
+    public:
+        // This constructor will take the raw SDL_Texture pointer.
+        SDLTexture(SDL_Texture* texture);
+        virtual ~SDLTexture();
+
+        int get_width() const override;
+        int get_height() const override;
+
+        // A public helper method for the SDLRenderer to get the underlying raw pointer,
+        // that it requires for SDL API calls.
+        SDL_Texture*  get_raw_texture() const;
+    
+    private:
+        SDL_Texture* texture;
+        int width;
+        int height;
+};
