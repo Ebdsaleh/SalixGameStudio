dc31a3c68187ae4f671db8e5765a66148c7a7ae2
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-16 16:15:57 +1000

    Added 'get_world_position', 'get_world_rotation', and 'get_world_scale' methods to 'Transform.h

--- src/ecs/Transform.h
+++ src/ecs/Transform.h
@@ -13,9 +13,15 @@ class Transform : public Element {
         // --- Public Data ---
         // Keeping these public for now, for the sake of simplicity.
 
+        // Local transform properties (relative to its parent)
         Vector3 position;
         Vector3 rotation;
         Vector3 scale;
+
+        // Calculate World Transform
+        Vector3 get_world_position();
+        Vector3 get_world_rotation();
+        Vector3 get_world_scale();
 
         // --- Hierarchy Methods ---
         void set_parent(Transform* new_parent);
