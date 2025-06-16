083c3c144c1b93834fb050fa42d5556a8132160f
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-16 22:41:43 +1000

    Added linear interpolation method to 'Vector3.h'.

--- src/math/Vector3.h
+++ src/math/Vector3.h
@@ -5,6 +5,9 @@ struct Vector3 {
     float x = 0.0f;
     float y = 0.0f;
     float z = 0.0f;
+
+    // static lerp method declaration
+    static Vector3 lerp(const Vector3& start, const Vector3& end, float t);
 };
 
 // Operator overloads
