729c40e8a37a3278e436f812f7c04b9418402527
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-16 17:39:36 +1000

    Updated 'Vector3.cpp' prevented division by zero operations, by zero-ing the offending axis, and printing a warning to the console.

--- src/math/Vector3.cpp
+++ src/math/Vector3.cpp
@@ -1,5 +1,6 @@
 // Vector3.cpp
 #include "Vector3.h"
+#include <iostream>  // Needed for std::cerr
 
 // Operator overloads
 // Adding Vector3's.
@@ -19,5 +20,27 @@ Vector3 operator*(const Vector3& a, const Vector3& b) {
 
 // Dividing Vector3's.
 Vector3 operator/(const Vector3& a, const Vector3& b) {
-    return {a.x / b.x, a.y / b.y, a.z / b.z};
+    // Check each axis individually to handle division by Zero safely.
+
+    float result_x = 0.0f;
+    if (b.x != 0.0f) {
+        result_x = a.x / b.x;
+    } else {
+        std::cerr << "Warning: Division by zero on X-axis!" << std::endl;
+    }
+
+    float result_y = 0.0f;
+    if (b.y !=0) {
+        result_y = a.y / b.y;
+    } else {
+        std::cerr << "Warning: Division by zero on Y-axis!" << std::endl;
+    }
+
+    float result_z = 0.0f;
+    if (b.z !=0) {
+        result_z = a.z / b.z;
+    } else {
+        std::cerr << "Warning: Division by zero on Z-axis!" << std::endl;
+    }
+    return { result_x, result_y, result_z };
 }
