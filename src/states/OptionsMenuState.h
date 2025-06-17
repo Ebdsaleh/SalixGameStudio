afedf10da79daa341743e0ce31301d208a0ca7ed
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-18 01:54:44 +1000

    Created 'OptionsMenuState.h'.

---
+++ src/states/OptionsMenuState.h
@@ -0,0 +1,16 @@
+
+ // OptionsMenuState.h
+#pragma once
+#include "IAppState.h"
+
+class OptionsMenuState : public IAppState {
+    public:
+    OptionsMenuState();
+    virtual ~OptionsMenuState();
+
+    void on_enter(class Engine* engine) override;
+    void on_exit() override;
+    void update(float delta_time) override;
+    void render(class IRenderer* renderer) override;
+
+};
