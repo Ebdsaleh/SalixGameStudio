49c7612cc11b9afe866a7ab9b0c115d8bd1f4b24
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-18 02:03:32 +1000

    Created 'OptionsMenuState.cpp'.

---
+++ src/states/OptionsMenuState.cpp
@@ -0,0 +1,22 @@
+// OptionsMenuState.cpp
+#include "OptionsMenuState.h"
+#include <iostream>
+
+OptionsMenuState::OptionsMenuState() {}
+OptionsMenuState::~OptionsMenuState() {}
+
+void OptionsMenuState::on_enter(Engine* engine) {
+    std::cout << "Entering OptionsMenuState..." << std::endl;
+}
+
+void OptionsMenuState::on_exit() {
+    std::cout << "Exiting OptionsMenuState..." << std::endl;
+}
+
+void OptionsMenuState::update(float delta_time) {
+    // Handle input for changing options here in the future.
+}
+
+void OptionsMenuState::render(IRenderer* renderer) {
+    // Render the options UI here in the future.
+}
