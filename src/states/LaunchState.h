d3b690b8ab184064653ad4da5bb5d0aca6a7e4b5
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-18 01:26:11 +1000

    Updated 'LaunchState.h'.

--- src/states/LaunchState.h
+++ src/states/LaunchState.h
@@ -4,13 +4,12 @@
 
 class LaunchState : public IAppState{
     public:
-    LaunchState();
-    virtual ~LaunchState();
+        LaunchState();
+        virtual ~LaunchState();
 
-    // Implement IAppState interface
-    void on_enter(class Engine* engine) override;
-    void on_exit() override;
-    void update(float delta_time) override;
-    void render(class IRenderer* renderer) override;
-
+        // Implement IAppState interface
+        void on_enter(class Engine* engine) override;
+        void on_exit() override;
+        void update(float delta_time) override;
+        void render(class IRenderer* renderer) override;
 };
