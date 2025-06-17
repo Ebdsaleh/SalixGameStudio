19e87ce36b88cacd9b0b3229bda8eabee3ee3ad6
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-18 05:09:19 +1000

    Created 'SDLTimer.h'.

---
+++ src/core/SDLTimer.h
@@ -0,0 +1,17 @@
+// SDLTimer.h
+#pragma once
+#include "ITimer.h"
+
+class SDLTimer : public ITimer {
+    public:
+        ~SDLTimer();
+        
+        void set_target_fps(int fps) override;
+        void tick() override;
+        float get_delta_time() const override;
+
+    private:
+    unsigned int last_tick_time;
+    float delta_time;
+    float target_frame_duration_ms;
+};
