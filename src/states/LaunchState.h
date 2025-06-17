0949fb96e0b54bece2ab92df1ce6eeef049ecbc2
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-18 01:01:20 +1000

    Created 'LaunchState.h'.

---
+++ src/states/LaunchState.h
@@ -0,0 +1,15 @@
+// LaunchState.h
+#pragma once
+#include "IAppState.h"
+
+class LaunchState : public IAppState{
+    LaunchState();
+    virtual ~LaunchState();
+
+    // Implement IAppState interface
+    void on_enter(class Engine* engine) override;
+    void on_exit() override;
+    void update(float delta_time) override;
+    void render(class IRenderer* renderer) override;
+
+};
