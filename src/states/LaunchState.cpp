a299aa628af17b8c0c2547d25aee652e8c7db591
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-19 13:52:07 +1000

    Updated 'LaunchState.cpp'. Doing a simple input is_down(KeyCode) test.

--- src/states/LaunchState.cpp
+++ src/states/LaunchState.cpp
@@ -1,12 +1,15 @@
 // LaunchState.cpp
 #include "LaunchState.h"
+#include "../core/Engine.h"
+#include "../input/IInputManager.h"
 #include <iostream>
-
-LaunchState::LaunchState() {}
+LaunchState::LaunchState() : engine_ptr(nullptr), input_manager_ptr(nullptr){}
 LaunchState::~LaunchState() {}
 
 void LaunchState::on_enter(Engine* engine) {
     std::cout << "Entering LaunchState..." << std::endl;
+    engine_ptr = engine;
+    input_manager_ptr = engine->get_input_manager();
 }
 
 void LaunchState::on_exit() {
@@ -15,7 +18,11 @@ void LaunchState::on_exit() {
 
 void LaunchState::update(float delta_time) {
     // In the future we'll check for button clicks here.
-    // For now, it does nothing.
+    if (input_manager_ptr->is_down(KeyCode::E)) std::cout << "'E' key press detected. Switching to EditorState." << std::endl;
+    
+    if (input_manager_ptr->is_down(KeyCode::G)) std::cout << "'G' key press detected.Switching to GameState." << std::endl;
+    
+    if (input_manager_ptr->is_down(KeyCode::O)) std::cout << "'O' key press detected.Switching to OptionsMenuState." << std::endl;
 }
 
 void LaunchState::render(IRenderer* renderer) {
