5ff0afdf20caf8c29a4d651d3fb5284d7bfd4eaf
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-19 15:42:04 +1000

    Added helpful comments to 'IInputManager.h'

--- src/input/IInputManager.h
+++ src/input/IInputManager.h
@@ -12,12 +12,25 @@ class IInputManager {
         virtual void update() = 0;
 
         // Keyboard Queries
+
+        // Returns true the moment the frame detects the input
         virtual bool is_down(KeyCode key) const = 0;
+
+        // Returns true if the input 'is still down' from the previous frame
         virtual bool is_held_down(KeyCode key) const = 0;
+
+        // Returns true if the input 'is held down' for the target_duration value (measured in seconds).
         virtual bool is_held_down_for(KeyCode key, int target_duration) const = 0;
-        virtual bool was_just_released(KeyCode key) const = 0;
+
+        // Returns true when the input goes from 'down to up' (is_down to is_up).
         virtual bool was_released(KeyCode key) const = 0;
+
+        //Returns true when the input goes from 'down to up, after being in the 'is_held_down' state.
+        virtual bool was_just_released(KeyCode key) const = 0;
+        
+        // Returns true if the input is not detect this frame. 
         virtual bool is_up(KeyCode key) const = 0;
+
         virtual bool multiple_are_down(std::vector<KeyCode>& keys) const = 0;
         virtual bool multiple_are_held_down(std::vector<KeyCode>& keys) const = 0;
         virtual bool multiple_are_held_down_for(std::vector<KeyCode>& keys, int target_duration) const = 0;
