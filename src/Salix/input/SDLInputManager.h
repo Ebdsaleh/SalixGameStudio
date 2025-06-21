17e8af3d2f803dc02d2e94186e57188c7d887766
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-21 13:24:11 +1000

    Updated signatures for multiple input detection methods in 'SDLInputManager.h'

--- src/Salix/input/SDLInputManager.h
+++ src/Salix/input/SDLInputManager.h
@@ -22,11 +22,11 @@ namespace Salix {
             bool is_held_down_for(KeyCode key, float duration) const override;
             bool was_released(KeyCode key) const override;
             bool is_up(KeyCode key) const override;
-            bool multiple_are_down(std::vector<KeyCode>& keys) const override;
-            bool multiple_are_held_down(std::vector<KeyCode>& keys) const override;
-            bool multiple_are_held_down_for(std::vector<KeyCode>& keys, float duration) const override;
-            bool multiple_were_released(std::vector<KeyCode>& keys) const override;
-            bool multiple_are_up(std::vector<KeyCode>& keys) const override;
+            bool multiple_are_down(const std::vector<KeyCode>& keys) const override;
+            bool multiple_are_held_down(const std::vector<KeyCode>& keys) const override;
+            bool multiple_are_held_down_for(const std::vector<KeyCode>& keys, float duration) const override;
+            bool multiple_were_released(const std::vector<KeyCode>& keys) const override;
+            bool multiple_are_up(const std::vector<KeyCode>& keys) const override;
 
             // Mouse Queries
             // Mouse Buttons
@@ -35,11 +35,11 @@ namespace Salix {
             bool is_held_down_for(MouseButton button, float duration) const override;
             bool was_released(MouseButton button) const override;
             bool is_up(MouseButton button) const override;
-            bool multiple_are_down(std::vector<MouseButton>& buttons) const override;
-            bool multiple_are_held_down(std::vector<MouseButton>& buttons) const override;
-            bool multiple_are_held_down_for(std::vector<MouseButton>& buttons, float duration) const override;
-            bool multiple_were_released(std::vector<MouseButton>& buttons) const override;
-            bool multiple_are_up(std::vector<MouseButton>& buttons) const override;
+            bool multiple_are_down(const std::vector<MouseButton>& buttons) const override;
+            bool multiple_are_held_down(const std::vector<MouseButton>& buttons) const override;
+            bool multiple_are_held_down_for(const std::vector<MouseButton>& buttons, float duration) const override;
+            bool multiple_were_released(const std::vector<MouseButton>& buttons) const override;
+            bool multiple_are_up(const std::vector<MouseButton>& buttons) const override;
 
             // Additional queries
             void get_mouse_position(int* x, int* y) const override;
