42876a257695b81715612fbc04701ad33b68a1ff
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-22 21:11:29 +1000

    Updated the Project class. Added and implemented methods : 'set_starting_scene', 'add_scene_path', 'remove_scene_path', 'get_name', 'get_path', 'get_starting_scene', and 'get_scene_paths'.

--- src/Salix/management/Project.cpp
+++ src/Salix/management/Project.cpp
@@ -1,11 +1,19 @@
 // Salix/management/Project.cpp
 #include <Salix/management/Project.h>
 #include <Salix/management/SceneManager.h>
+#include <Salix/management/FileManager.h>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <algorithm>
 
 namespace Salix {
     struct Project::Pimpl {
         std::unique_ptr<SceneManager> scene_manager;
+        std::string name;
+        std::string root_path;
+        std::string starting_scene;
+        std::vector<std::string> scene_paths;
     };
     Project::Project() : pimpl(std::make_unique<Pimpl>()) {}
     Project::~Project() = default;
@@ -35,7 +43,53 @@ namespace Salix {
         }
     }
 
+    void Project::set_starting_scene(const std::string& scene_name) {
+        pimpl->starting_scene = scene_name;
+    }
+    
+    void Project::add_scene_path(const std::string& path_to_scene_file) {
+        pimpl->scene_paths.push_back(path_to_scene_file);
+    }
+
+    bool Project::remove_scene_path(const std::string& path_to_scene_file) {
+        if(!FileManager::path_exists(path_to_scene_file)) {
+            std::cerr << "Project::remove_scene_path - Failed to remove scene path: '" <<
+             path_to_scene_file << "', does not exist!" << std::endl;
+            return false;
+        }
+        auto& scene_paths = pimpl->scene_paths;
+        // Check the size before and after to see if an element was removed.
+        const auto original_size = scene_paths.size();
+        
+        // The "erase-remove idiom":
+        // 1. std::remove shuffles all elements that are NOT the one we want to remove
+        //    to the beginning of the vector. It returns an iterator to the new "end".
+        // 2. .erase() then chops off the garbage elements at the end.
+        scene_paths.erase(
+            std::remove(scene_paths.begin(), scene_paths.end(), path_to_scene_file),
+            scene_paths.end()
+        );
+        // If the new size is less than the original, it means we successfully removed something.
+        return scene_paths.size() < original_size;
+
+    }
+    
+    // --- Public Getter Methods ---
     SceneManager* Project::get_scene_manager() const {
         return pimpl->scene_manager.get();
     }
-} // namespace Salix
+
+    const std::string& Project::get_name() const {
+
+        return pimpl->name;
+    }
+    const std::string& Project::get_path() const {
+        return pimpl->root_path;
+    }
+    const std::string& Project::get_starting_scene() const {
+        return pimpl->starting_scene;
+    }
+    const std::vector<std::string>& Project::get_scene_paths() const {
+        return pimpl->scene_paths;
+    }
+    } // namespace Salix
--- src/Salix/management/Project.h
+++ src/Salix/management/Project.h
@@ -10,6 +10,7 @@ namespace Salix {
     class SceneManager;
     class IRenderer;
     class AssetManager;
+    class FileManager;
 
     class SALIX_API Project {
     public:
@@ -21,7 +22,20 @@ namespace Salix {
         void update(float delta_time);
         void render(IRenderer* renderer);
         
+        
+        // These methods allow the ProjectManager to populate this object with
+        // data that would normally be read from a project file.
+        void set_starting_scene(const std::string& scene_name);
+        void add_scene_path(const std::string& path_to_scene_file);
+        bool remove_scene_path(const std::string& path_to_scene_file);
+
+        // --- Public Getters ---
         SceneManager* get_scene_manager() const;
+        const std::string& get_name() const;
+        const std::string& get_path() const;
+        const std::string& get_starting_scene() const;
+        const std::vector<std::string>& get_scene_paths() const;
+
 
     private:
         struct Pimpl;
