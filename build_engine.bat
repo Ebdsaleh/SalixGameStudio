599307f1a1fbb27a3d462c902cfcc3367289757d
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-23 04:57:59 +1000

    Added  '/I "%cd%\vendor\nlohmann ^" ' includes to 'build_engine.bat', and 'build_game.bat'.

--- build_engine.bat
+++ build_engine.bat
@@ -31,6 +31,7 @@ cl.exe ^
     /Fe"%BUILD_DIR%\%ENGINE_EXE_NAME%" ^
     /I "%SDL2_PATH%\include" ^
     /I "%SDL2_IMAGE_PATH%\include" ^
+    /I "%cd%\vendor\nlohmann_json" ^
     /I "src" ^
     %ENGINE_SOURCES% ^
     /link ^
