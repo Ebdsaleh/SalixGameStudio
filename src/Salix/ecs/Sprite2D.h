224d81f71e5d755ec82dfe2df142f1e56f0e6196
Ebdsaleh <nadirsaleh@gmail.com>
2025-06-24 17:26:32 +1000

    Updated 'Sprite2D.h' Implemented Cereal serializtion by adding
    'template
    
    void serialize(Archive& archive)' method.

--- src/Salix/ecs/Sprite2D.h
+++ src/Salix/ecs/Sprite2D.h
@@ -7,6 +7,8 @@
 #include <Salix/math/Vector2.h>  // For the pivot and offset.
 #include <string>
 #include <memory>
+#include <cereal/cereal.hpp>
+#include <cereal/types/string.hpp> // Needed to serialize std::string
 
 namespace Salix {
 
@@ -22,8 +24,12 @@ namespace Salix {
             // A method to load a texture for this sprite using the AssetManager
             void load_texture(class AssetManager* asset_manager, const std::string& file_path);
 
+            // A method to return the path of the assigned texture.
+            const std::string& get_texture_path() const;
+
             // The implementation of the render method from RenderableElement Interface.
             void render(IRenderer* renderer) override;
+
 
             // --- Properties ---
             
@@ -34,11 +40,16 @@ namespace Salix {
             bool flip_v = false;    // Flip veritcally?
             int sorting_layer = 0;  // For future use in the Scene.
 
+            template<class Archive>
+            void serialize(Archive& archive) {
+                archive ( CEREAL_NVP(color), CEREAL_NVP(offset),
+                    CEREAL_NVP(pivot), CEREAL_NVP(flip_h), CEREAL_NVP(flip_v),
+                    CEREAL_NVP(sorting_layer),
+                    cereal::make_nvp("texture_path", get_texture_path()) );
+            }
 
         private:
             struct Pimpl;
             std::unique_ptr<Pimpl>pimpl;
-            
-
     };
 } // namespace Salix
