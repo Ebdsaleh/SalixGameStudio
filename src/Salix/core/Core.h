61e520d4ab83cec7c93cbcf25e4d23ea30ecc06c
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-01 05:08:59 +1000

    Added 'g_project_root_path' a global runtime variable.

--- src/Salix/core/Core.h
+++ src/Salix/core/Core.h
@@ -6,6 +6,7 @@
 // =================================================================================
 #pragma once
 #include <Salix/core/EngineInfo.h>
+#include <filesystem>
 #ifdef _WIN32
     // --- FOR THE ENGINE ---
     #ifdef SALIX_BUILD_ENGINE // Defined when building the Engine exe
@@ -23,3 +24,10 @@
 #else
     #error Salix Engine only supports Windows for now!
 #endif
+
+namespace Salix {
+    // Declares the global project root path variable, making it accessible
+    // across the EXE/DLL boundary.
+    // This is a DECLARATION. 'extern' promises it's defined elsewhere.
+    extern SALIX_API std::filesystem::path g_project_root_path;
+}
