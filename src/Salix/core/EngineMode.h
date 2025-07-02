28ce046c70a6a94aaf2e02b0037866a47ac043cd
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-02 19:57:50 +1000

    Created 'EngineMode.h' and moved the 'EngineMode' enum class into its own file.

---
+++ src/Salix/core/EngineMode.h
@@ -0,0 +1,13 @@
+// Salix/core/EngineMode.h
+#pragma once
+
+namespace Salix {
+    enum class EngineMode {
+        None,
+        Launch,
+        Editor,
+        Game,
+        Options,
+        Debug
+    };
+}
