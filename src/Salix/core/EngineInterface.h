bc0f67c72aa1580c8e86b1452b7da120c1db045a
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-07 07:06:53 +1000

    Updated 'EngineInterface.h' to get and set the 'is_running' flag in the engine. This allows to any State to call shut the engine down safely.

--- src/Salix/core/EngineInterface.h
+++ src/Salix/core/EngineInterface.h
@@ -5,12 +5,15 @@
 #include <Salix/core/EngineMode.h>
 
 namespace Salix {
-
+    
     class SALIX_API EngineInterface {
     public:
         virtual void switch_state(AppStateType state) = 0;
         virtual void set_mode(EngineMode mode) = 0;
         virtual EngineMode get_mode() const = 0;
         virtual ~EngineInterface() = default;
+        virtual bool is_running() const = 0;
+        virtual const bool is_running(bool keep_running) = 0;
+
     };
 } // namespace Salix
