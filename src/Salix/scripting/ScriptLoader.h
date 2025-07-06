295f3395ab35fdb5eeafd87a13493b271246a6ea
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-07 07:45:18 +1000

    Updated 'ScriptLoader.h' to make it a part of the SALIX_API, and added a 'shutdown_plugin' method declaration.

--- src/Salix/scripting/ScriptLoader.h
+++ src/Salix/scripting/ScriptLoader.h
@@ -1,11 +1,14 @@
 // Salix/scripting/ScriptLoader
 #pragma once
+#include <Salix/core/Core.h>
 #include <string>
 
 namespace Salix {
-    class ScriptLoader {
+    struct InitContext;
+    class SALIX_API ScriptLoader {
     public:
         // Attempts to load the script DLL and call any dynamic initializers
-        static bool load_script_library(const std::string& path);
+        static bool load_script_library(const std::string& path, InitContext& context);
+        void shutdown_plugin();
     };
 }
