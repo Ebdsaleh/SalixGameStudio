25e3a5e2822b4feee09f3f03370d3b5da3e15213
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-07 07:17:54 +1000

    Created  'InitEnums.h' This file contains the majority of enum classes required during Engine initialization.

---
+++ src/Salix/core/InitEnums.h
@@ -0,0 +1,31 @@
+// Salix/core/InitEnums.h
+#pragma once
+
+namespace Salix {
+
+  // An enum for declaring the type of renderer in use.
+    enum class RendererType {
+        None, // initial setting, usually won't ever be set to this unless there's a specific reason.
+        SDL, // The default option
+        OpenGl, // OpenGl using the SDL API
+        Vulkan,  // For the future
+        DirectX, // For the future
+    };
+
+    enum class TimerType {
+        SDL,
+        Chrono
+    };
+    enum class GuiType {
+        None,       //  For GameState inside user projects executable.
+        ImGui,      // DearImGui
+    };
+
+     enum class AppStateType {
+        None,
+        Launch,
+        Editor,
+        Game,
+        Options
+    };
+}  // namespace Salix
