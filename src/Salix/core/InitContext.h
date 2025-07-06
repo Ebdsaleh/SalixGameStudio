eb2066c7a037fae1c47856b3b30e8f823ffd2a11
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-07 07:15:03 +1000

    Added more members to the 'InitContext' struct to allow states to access them safely. These include IWindow, ITimer, IGui, IEventPoller, and IEventManager classes. Also added two std::string variables 'project_file_path', and 'project_dll_path', for keeping track of project information between states.

--- src/Salix/core/InitContext.h
+++ src/Salix/core/InitContext.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <Salix/core/Core.h>
+#include <Salix/core/InitEnums.h>
 #include <Salix/core/EngineInterface.h>
 #include <Salix/core/EngineMode.h>  // For EngineMode enum
+#include <string>
 
 // Forward declarations to avoid circular dependencies
 namespace Salix {
@@ -8,12 +11,27 @@ namespace Salix {
     class AssetManager;
     class IInputManager;
     class IRenderer;
+    class IWindow;
+    class ITimer;
+    class IGui;
+    class IEventPoller;
+    class IEventManager;
 
-    struct InitContext {
+    struct SALIX_API InitContext {
+        RendererType  renderer_type = RendererType::None;
+        GuiType gui_type = GuiType::None;
+        IGui* gui;
+        ITimer* timer;
+        TimerType timer_type;
         EngineInterface* engine = nullptr;
         AssetManager* asset_manager = nullptr;
         IInputManager* input_manager = nullptr;
         IRenderer* renderer = nullptr;
+        IWindow* window = nullptr;
+        IEventPoller* event_poller;
+        IEventManager* event_manager;
         EngineMode engine_mode = EngineMode::None;
+        std::string project_file_path;
+        std::string  project_dll_path;
     };
 }
