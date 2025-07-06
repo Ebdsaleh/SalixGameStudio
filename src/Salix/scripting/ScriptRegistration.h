f6b75bf9c51ff57d3f15b711cc49ef1ab84e9254
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-07 07:43:26 +1000

    Updated 'ScriptRegistration.h' Still trying to automate Cereal unknown Cereal declarations and definitions.

--- src/Salix/scripting/ScriptRegistration.h
+++ src/Salix/scripting/ScriptRegistration.h
@@ -1,31 +1,36 @@
 // Salix/scripting/ScriptRegistration.h
 #pragma once
-
+#include <Salix/core/Core.h>
+#include <Salix/core/SerializationRegistrations.h>
 #include <Salix/scripting/ScriptFactory.h>
 #include <cereal/types/polymorphic.hpp>
-#include <memory> // For std::make_unique
+#include <memory>
 
-// Salix/scripting/ScriptRegistration.h
-#pragma once
+// --- Registers with Cereal ---
+#define REGISTER_SCRIPT_TYPE(T) \
+    CEREAL_REGISTER_TYPE(T); \
+    CEREAL_REGISTER_POLYMORPHIC_RELATION(Salix::CppScript, T);
 
-#include <Salix/scripting/ScriptFactory.h>
-#include <cereal/types/polymorphic.hpp>
-#include <memory> // For std::make_unique
+// --- Registers with the ScriptFactory ---
+#define REGISTER_SCRIPT_FACTORY(T) \
+    namespace { \
+        struct AutoRegister_##T { \
+            AutoRegister_##T() { \
+                Salix::ScriptFactory::get().register_script(#T, []() { \
+                    return std::make_unique<T>(); \
+                }); \
+            } \
+        }; \
+        static AutoRegister_##T g_auto_register_##T; \
+    }
 
-// Move Cereal registration macros outside the constructor.
-// These macros need to be in the global or namespace scope.
-#define REGISTER_SCRIPT(T) \
-    struct Registerer_##T { \
-        Registerer_##T() { \
-            Salix::ScriptFactory::get().register_script(#T, []() { \
-                return std::make_unique<T>(); \
-            }); \
-        } \
-    }; \
-    static Registerer_##T g_registerer_##T; \
-    //CEREAL_REGISTER_TYPE(T); \
-    // CEREAL_REGISTER_POLYMORPHIC_RELATION(Salix::CppScript, T); \
-    // CEREAL_REGISTER_DYNAMIC_INIT(T);
+
+
+// ------------------------------------------------------------------------------------
+// SCRIPT_SERIALIZATION_BODY(T)
+// ------------------------------------------------------------------------------------
+// Adds the basic serialize method (archiving base class only)
+// ------------------------------------------------------------------------------------
 
 #define SCRIPT_SERIALIZATION_BODY(T) \
 private: \
