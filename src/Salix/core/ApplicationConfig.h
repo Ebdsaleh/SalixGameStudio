6f0ec5fbb6b7d269b357fde2b0d95dfe22441bbe
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-12 15:59:29 +1000

    Updated 'ApplicationSettings.h' Added 'GuiSettings' struct for use with 'DialogBox' default sizing acrross different resolutions.

--- src/Salix/core/ApplicationConfig.h
+++ src/Salix/core/ApplicationConfig.h
@@ -6,6 +6,13 @@
 
 namespace Salix {
 
+    // A struct to hold GUI-related settings that can be configured.
+    struct GuiSettings {
+        // Defines dialog size as a percentage of the main window's dimensions.
+        float dialog_width_ratio = 0.7f;  // Default to 70% of window width
+        float dialog_height_ratio = 0.75f; // Default to 75% of window height
+    };
+
     struct ApplicationConfig { 
         WindowConfig window_config;
         RendererType renderer_type = RendererType::SDL;
@@ -13,6 +20,7 @@ namespace Salix {
         GuiType gui_type = GuiType::ImGui;
         TimerType timer_type = TimerType::SDL;
         int target_fps = 60;
+        GuiSettings gui_settings;
 
 
     };
