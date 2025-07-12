60e7cf523ea54dcd971193b790eb10a26dd506a3
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-12 15:17:55 +1000

    Updated 'WindowConfig.h' , changed the 'title' property to a std::string from a const char*. This was necessary for using the 'yaml-cpp ' submodule.

--- src/Salix/window/WindowConfig.h
+++ src/Salix/window/WindowConfig.h
@@ -1,11 +1,12 @@
 // Salix/window/WindowConfig.h
 #pragma once
 #include <Salix/core/Core.h>
+#include <string>
 
 namespace Salix {
     // A simple struct to hold window configuration.
     struct SALIX_API WindowConfig {
-        const char* title = "SalixGameStudio";
+        std::string title = "SalixGameStudio";
         int width = 1280;
         int height = 720;
     };
