7401de9d0ff37f8be533f53af13c013cea6100aa
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-15 19:36:09 +1000

    Created 'IPanel.h' - IPanel interface base-class for Editor-specific Imgui visual component zones.

---
+++ src/Editor/panels/IPanel.h
@@ -0,0 +1,28 @@
+// =================================================================================
+// Filename:    Editor/panels/IPanel.h
+// Author:      SalixGameStudio
+// Description: Defines the abstract interface for all editor panels.
+// =================================================================================
+#pragma once
+#include <Editor/EditorAPI.h>
+#include <imgui/imgui.h>
+namespace Salix {
+
+    struct EditorContext;
+
+    class EDITOR_API IPanel {
+    public:
+        // A virtual destructor is essential for any class intended for polymorphism.
+        virtual ~IPanel() = default;
+
+        virtual void initialize(EditorContext* context) = 0;
+
+        // This is the core function for any panel.
+        // The editor will call this every frame, and the panel is responsible
+        // for all of its ImGui rendering code inside this function.
+        virtual void on_gui_render() = 0;
+        virtual void set_visibility(bool visibility) = 0;
+        const virtual bool get_visibility() const = 0;
+    };
+
+} // namespace Salix
