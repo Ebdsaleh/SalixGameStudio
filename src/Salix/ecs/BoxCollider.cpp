ecbc5bcee86a1d4d454db66d5908c441cd4a01b9
Ebdsaleh <nadirsaleh@gmail.com>
2025-07-30 16:23:41 +1000

    Created 'BoxCollider' Element class - This assists in collision/raycasting detection, Applied to all Entity's by default now.

---
+++ src/Salix/ecs/BoxCollider.cpp
@@ -0,0 +1,43 @@
+// Salix/ecs/BoxCollider.cpp
+#include <Salix/ecs/BoxCollider.h>
+#include <Salix/core/InitContext.h>
+#include <Salix/math/Vector3.h>
+#include <iostream>
+#include <memory>
+
+namespace Salix {
+
+    struct BoxCollider::Pimpl { 
+        Vector3 size = Vector3(1.0f, 1.0f, 1.0f);
+        const char* class_name = "BoxCollider";
+    };
+
+    BoxCollider::BoxCollider() :pimpl(std::make_unique<Pimpl>()) {}
+    BoxCollider::~BoxCollider() = default;
+
+    void BoxCollider::initialize() {}
+
+    void BoxCollider::on_load(const InitContext& context) {
+        if (!context.renderer) return;
+
+    }
+
+
+    void BoxCollider::update(float delta_time) {
+        (void) delta_time;
+    }
+
+
+    const char* BoxCollider::get_class_name() const {
+        return pimpl->class_name;
+    }
+
+    const Vector3 BoxCollider::get_size() {
+        return pimpl->size;
+    }
+
+    void BoxCollider::set_size(Vector3 new_size) {
+        pimpl->size = new_size;
+    }
+
+}
---
+++ src/Salix/ecs/BoxCollider.h
@@ -0,0 +1,30 @@
+// Salix/ecs/BoxCollider.h
+#pragma once
+#include <Salix/core/Core.h>
+#include <Salix/ecs/Element.h>
+#include <Salix/math/Vector3.h>
+#include <memory>
+
+namespace Salix {
+
+    struct InitContext;
+    class SALIX_API BoxCollider : public Element {
+    public:
+
+
+        BoxCollider();
+        ~BoxCollider() override;
+        const char* get_class_name() const override;
+        void initialize() override;
+        void on_load(const InitContext& context) override;
+        void update(float delta_time) override;
+        const Vector3 get_size();
+        void set_size(Vector3 new_size);
+        
+       
+    private:
+        struct Pimpl;
+        std::unique_ptr<Pimpl> pimpl;
+    };
+
+}
