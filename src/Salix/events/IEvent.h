7db1a7c391d992659458b3dbe211ecebd29643c7
Ebdsaleh <nadirsaleh@gmail.com>
2025-08-04 21:33:10 +1000

    Updated 'IEvent.h' - added new event 'BeforeEnityPurged'. This is called when an Entity is set to be purged (deleted/destroyed) this allows for preparation  of safe clean up.

--- src/Salix/events/IEvent.h
+++ src/Salix/events/IEvent.h
@@ -42,7 +42,7 @@ namespace Salix {
         None, WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
         AppTick, AppUpdate, AppRender, KeyPressed, KeyReleased, KeyTyped,
         MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled,
-        ImGuiInput, EditorEntitySelected, EditorElementSelected, EditorThemeReloadEvent
+        ImGuiInput, EditorEntitySelected, EditorElementSelected, EditorThemeReloadEvent,BeforeEntityPurged
     };
 
     class SALIX_API IEvent {
