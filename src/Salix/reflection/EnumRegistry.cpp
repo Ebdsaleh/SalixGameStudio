abf17875ae34a4483983fa98602b2e40349b555c
Ebdsaleh <nadirsaleh@gmail.com>
2025-08-05 21:18:01 +1000

    Updated 'EnumRegistry.cpp' - Added and registered EnumData for 'EditorDataMode'.

--- src/Salix/reflection/EnumRegistry.cpp
+++ src/Salix/reflection/EnumRegistry.cpp
@@ -1,5 +1,6 @@
 // Salix/reflection/EnumRegistry.cpp
 #include <Salix/reflection/EnumRegistry.h>
+#include <Salix/reflection/EditorDataMode.h>
 #include <Salix/rendering/ICamera.h>
 #include <Salix/ecs/Camera.h>
 
@@ -7,6 +8,7 @@ namespace Salix {
     std::unordered_map<std::type_index, EnumRegistry::EnumData> EnumRegistry::enum_data_registry;
 
     void  EnumRegistry::register_all_enums() {
+        // ProjectionMode
         EnumRegistry::EnumData projection_mode_data;
 
         // Populate value_to_string map
@@ -19,5 +21,15 @@ namespace Salix {
 
         // Register the enum data
         EnumRegistry::register_enum(typeid(Salix::ProjectionMode), std::move(projection_mode_data));
+
+
+
+        // --- Register EditorDataMode enum data ---
+        EnumRegistry::EnumData editor_mode_data;
+        editor_mode_data.value_to_string[static_cast<int>(Salix::EditorDataMode::Live)] = "Live";
+        editor_mode_data.value_to_string[static_cast<int>(Salix::EditorDataMode::Yaml)] = "Yaml";
+        editor_mode_data.ordered_names.push_back("Live");
+        editor_mode_data.ordered_names.push_back("Yaml");
+        EnumRegistry::register_enum(typeid(Salix::EditorDataMode), std::move(editor_mode_data));
     }
 }
