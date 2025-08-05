8ce6c420b38c50eb6bf128b270668ff8e85e7841
Ebdsaleh <nadirsaleh@gmail.com>
2025-08-05 15:36:54 +1000

    Updated 'Element.h' - Added SimpleGuid member 'id' and call to generate a new id upon instantiation. Updated Cereal-enabled template class 'serialize', now allowing serialization for the 'id' property.

--- src/Salix/ecs/Element.h
+++ src/Salix/ecs/Element.h
@@ -2,6 +2,7 @@
 #pragma once
 #include <Salix/core/Core.h>
 #include <Salix/core/InitContext.h>
+#include <Salix/core/SimpleGuid.h>
 #include <cereal/cereal.hpp>
 
 namespace Salix {
@@ -11,7 +12,7 @@ namespace Salix {
 
     class SALIX_API Element {
         public:
-            Element() = default;
+            Element() : id(SimpleGuid::generate()) {}
             // A virtual destructor is essential for any class with virtual methods
             virtual ~Element() = default;
             // NEW: Add a pure virtual function to get the element's type name.
@@ -28,10 +29,11 @@ namespace Salix {
             void set_owner(Entity* owner_entity) {
                 owner = owner_entity;
             }
-
-           
+            
+            const SimpleGuid&  get_id() const {return id;}
             template <class Archive>
-            void serialize(Archive& /* archive*/) {
+            void serialize(Archive& archive) {
+                archive(cereal::make_nvp("id", id));
                 /// The base class of the hierarchy has no parent to serialize.
                 // Since it has no data of its own, this function is empty.
                 // It MUST exist, however, to complete the polymorphic chain.
@@ -40,7 +42,7 @@ namespace Salix {
             // A pointer to the Entity that owns this element.
             // This allowse elements to communicate with each other.
             Entity* owner = nullptr;
-
+            SimpleGuid id;
         private:
             // The Entity class that will need to be able to set the owner.
             friend class Entity;
