6acdcb3cab2ec344b11a1ceb405db79cd7da4f25
Ebdsaleh <nadirsaleh@gmail.com>
2025-08-11 02:21:38 +1000

    Updated 'ElementSelectedEvent.h' - Added two new SimpleGuid members 'owner_id', and 'selected_id'. Modified the constructor to require the id of the Element and its owner's id, as well as making the referenced Element* optional.

--- src/Editor/events/ElementSelectedEvent.h
+++ src/Editor/events/ElementSelectedEvent.h
@@ -2,6 +2,7 @@
 
 #include <Salix/events/IEvent.h>
 #include <Salix/ecs/Element.h> 
+#include <Salix/core/SimpleGuid.h>
 
 #ifndef EVENT_CLASS_TYPE
 #define EVENT_CLASS_TYPE(type) static EventType get_static_type() { return EventType::type; }\
@@ -16,12 +17,15 @@ namespace Salix {
 
 class ElementSelectedEvent : public IEvent {
     public:
-        ElementSelectedEvent(Element* selected_element)
-            : element(selected_element) {}
+        ElementSelectedEvent(SimpleGuid id, SimpleGuid owning_entity_id, Element* live_element = nullptr)
+            : selected_id(id), owner_id(owning_entity_id), element(live_element) {}
 
         EVENT_CLASS_TYPE(EditorElementSelected) 
         EVENT_CLASS_CATEGORY(EventCategory::Editor)
         Element* element;
+        // The stable ID, which works in both modes
+        SimpleGuid owner_id;
+        SimpleGuid selected_id;
     };
 
 } // namespace Salix
