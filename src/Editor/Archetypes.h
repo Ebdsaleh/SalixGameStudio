dc8fc510a8d8ed51358b1856a800c5dc4b7ef6ed
Ebdsaleh <nadirsaleh@gmail.com>
2025-08-16 00:54:52 +1000

    Updated 'Archetypes.h' - Added new enum class 'ArchetypeState', this allows for tracking when an Archetype has been modified, allowing for better user feedback when creating their Realms/Scenes.

--- src/Editor/Archetypes.h
+++ src/Editor/Archetypes.h
@@ -12,12 +12,19 @@
 
 namespace Salix {
 
+    enum class EDITOR_API ArchetypeState {
+        Unmodified, // Loaded from file, unchanged (White)
+        New,        // Created this session, unsaved (Green)
+        Modified    // Loaded from file, changed this session (Orange)
+    };
+
     struct EDITOR_API ElementArchetype {
         // The type of the element, e.g., "Transform", "Sprite2D".
         std::string type_name;
         std::string name;
         SimpleGuid id = SimpleGuid::invalid();
         bool allows_duplication = true;
+        ArchetypeState state = ArchetypeState::Unmodified;
         // The raw YAML data for this element's properties.
         YAML::Node data;
         // Empty default constructor
@@ -35,7 +42,7 @@ namespace Salix {
        
         // A list of its element archetypes instead of live elements
         std::vector<ElementArchetype> elements;
-        
+        ArchetypeState state = ArchetypeState::Unmodified;
         // Empty default constructor
         EntityArchetype() : id(SimpleGuid::invalid()){}
         
