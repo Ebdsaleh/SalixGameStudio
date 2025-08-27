e202064d9dc96ec45675ee6b6e04ee6b781b80ef
Ebdsaleh <nadirsaleh@gmail.com>
2025-08-27 23:27:37 +1000

    Updated 'IRenderer.h' - Modified overloaded 'draw_sprite' method signature, no longer requiring the 'SpriteFlip' enum as it's handled by the 'model_matrix'.

--- src/Salix/rendering/IRenderer.h
+++ src/Salix/rendering/IRenderer.h
@@ -83,8 +83,8 @@ namespace Salix {
         virtual void draw_texture(ITexture* texture, const Rect& dest_rect) = 0;
 
         virtual void draw_sprite(ITexture* texture, const Transform* transform, const Color& color, SpriteFlip flip) = 0;
-        virtual void draw_sprite(ITexture* texture, const glm::mat4& model_matrix, const Color& color, SpriteFlip flip) {
-            (void)texture; (void)model_matrix; (void)color; (void)flip;
+        virtual void draw_sprite(ITexture* texture, const glm::mat4& model_matrix, const Color& color) {
+            (void)texture; (void)model_matrix; (void)color;
         }
         virtual void draw_wire_box(const glm::mat4& model_matrix, const Color& color) = 0;
         virtual void draw_line(const glm::vec3& start, const glm::vec3& end, const Color& color) = 0; 
