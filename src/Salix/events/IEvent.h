b197b8bb95e11b2e9a9ff5f622bd66df22e55a65
Ebdsaleh <nadirsaleh@gmail.com>
2025-08-28 20:06:47 +1000

    Updated 'IEvent.h' - Added new event 'OnHierarchyChangedEvent'

--- src/Salix/events/IEvent.h
+++ src/Salix/events/IEvent.h
@@ -43,7 +43,7 @@ namespace Salix {
         AppTick, AppUpdate, AppRender, KeyPressed, KeyReleased, KeyTyped,
         MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled,
         ImGuiInput, EditorEntitySelected, EditorElementSelected, EditorThemeReloadEvent,BeforeEntityPurged,
-        EditorPropertyValueChanged
+        EditorPropertyValueChanged, EditorOnHierarchyChanged
     };
 
     class SALIX_API IEvent {
