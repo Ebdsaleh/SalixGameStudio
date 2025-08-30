ef5d6b8bed1029afae82a10c20ec32528ef0bbee
Ebdsaleh <nadirsaleh@gmail.com>
2025-08-31 04:11:24 +1000

    Created 'OnRootEntityAddedEvent.h' - An event to signal new Root-Level Entity has been created.

---
+++ src/Editor/events/OnRootEntityAddedEvent.h
@@ -0,0 +1,28 @@
+// Editor/events/OnRootEntityAddedEvent.h
+#pragma once
+
+#include <Salix/events/IEvent.h>
+#include <Editor/Archetypes.h>
+
+#ifndef EVENT_CLASS_TYPE
+#define EVENT_CLASS_TYPE(type) static EventType get_static_type() { return EventType::type; }\
+                                 virtual EventType get_event_type() const override { return get_static_type(); }\
+                                 virtual const char* get_name() const override { return #type; }
+#endif
+#ifndef EVENT_CLASS_CATEGORY
+#define EVENT_CLASS_CATEGORY(category) virtual int get_category_flags() const override { return static_cast<int>(category); }
+#endif
+
+namespace Salix {
+    // Dispatched AFTER a new ROOT entity has been added to the current_realm
+    class OnRootEntityAddedEvent : public IEvent {
+    public:
+        OnRootEntityAddedEvent(EntityArchetype new_archetype)
+            : archetype(std::move(new_archetype)) {}
+
+        EntityArchetype archetype;
+
+        EVENT_CLASS_TYPE(EditorOnRootEntityAdded)
+        EVENT_CLASS_CATEGORY(EventCategory::Editor)
+    };
+}
