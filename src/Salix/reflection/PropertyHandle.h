6f1be50ba3e452ca3f6c47c4f97909a48cd667fd
Ebdsaleh <nadirsaleh@gmail.com>
2025-09-01 22:46:46 +1000

    Updated 'PropertyHandle.h' - Refactored file by removing redundant #includes and moved the inline '<<' operator overload function to 'ReflectionTypes.h'.

--- src/Salix/reflection/PropertyHandle.h
+++ src/Salix/reflection/PropertyHandle.h
@@ -3,35 +3,11 @@
 
 #include <Salix/core/Core.h>
 #include <Salix/reflection/ByteMirror.h>
-#include <Salix/math/Vector2.h>
-#include <Salix/math/Vector3.h>
-#include <Salix/math/Color.h>
-#include <Salix/math/Point.h>
-#include <Salix/math/Rect.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <vector>
-#include <string>
-#include <ostream>
-#include <variant>
-#include <Salix/rendering/ICamera.h> 
+#include <Salix/reflection/ReflectionTypes.h>
+
 
 namespace Salix {
-
-    // A variant to hold any possible type a property could be.
-    // This now becomes the primary way we pass data.
-    using PropertyValue = std::variant<
-        int, uint64_t, float, bool, std::string, Vector2, Vector3, Color, Point, Rect, glm::mat4
-        // Note: You must add every type from your PropertyType enum here
-    >;
-    inline std::ostream& operator<<(std::ostream& os, const Salix::PropertyValue& value) {
-        // std::visit calls the correct lambda based on the type currently held by the variant.
-        std::visit([&os](const auto& val) {
-            os << val;
-        }, value);
-        // Return the stream to allow for chaining (e.g., std::cout << val1 << val2;)
-        return os;
-    }
+   
 
     // Abstract base class for a generic property handle.
     class SALIX_API PropertyHandle {
