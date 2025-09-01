c8aa4202855fe4f6019e35662489c7a3a175f8e2
Ebdsaleh <nadirsaleh@gmail.com>
2025-09-01 19:44:56 +1000

    Created 'OnChildEntityAddedEvent.h' - An event to signal that a child Entity has been added.

---
+++ src/Editor/events/OnChildEntityAddedEvent.h
@@ -0,0 +1,29 @@
+// Create a new file: Editor/events/OnChildEntityAddedEvent.h
+#pragma once
+
+#include <Salix/events/IEvent.h>
+#include <Editor/Archetypes.h>
+
+// Macros for event boilerplate
+#ifndef EVENT_CLASS_TYPE
+#define EVENT_CLASS_TYPE(type) static EventType get_static_type() { return EventType::type; }\
+                                 virtual EventType get_event_type() const override { return get_static_type(); }\
+                                 virtual const char* get_name() const override { return #type; }
+#endif
+#ifndef EVENT_CLASS_CATEGORY
+#define EVENT_CLASS_CATEGORY(category) virtual int get_category_flags() const override { return static_cast<int>(category); }
+#endif
+
+namespace Salix {
+    // Dispatched AFTER a new CHILD entity has been added to the current_realm
+    class OnChildEntityAddedEvent : public IEvent {
+    public:
+        OnChildEntityAddedEvent(EntityArchetype new_archetype)
+            : archetype(std::move(new_archetype)) {}
+
+        EntityArchetype archetype;
+
+        EVENT_CLASS_TYPE(EditorOnChildEntityAdded) 
+        EVENT_CLASS_CATEGORY(EventCategory::Editor)
+    };
+}
