b5e4f8dfbe862cb3f02219d1618a279e6958b83e
Ebdsaleh <nadirsaleh@gmail.com>
2025-09-03 06:18:40 +1000

    Updated 'BeforeElementPurgedEvent.h' - Added the 'CLONE_EVENT_METHOD' macro.

--- src/Salix/events/BeforeElementPurgedEvent.h
+++ src/Salix/events/BeforeElementPurgedEvent.h
@@ -23,5 +23,6 @@ namespace Salix {
 
         EVENT_CLASS_TYPE(BeforeElementPurged) // Add BeforeElementPurged to your EventType enum
         EVENT_CLASS_CATEGORY(EventCategory::Editor)
+        CLONE_EVENT_METHOD(BeforeElementPurgedEvent)
     };
 }
