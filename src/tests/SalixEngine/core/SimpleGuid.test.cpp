24b231f6766e1fca29070a1db2e1eff3ed39b1a7
Ebdsaleh <nadirsaleh@gmail.com>
2025-09-04 21:11:13 +1000

    Created 'SimpleGuid.test.cpp' this file tests the SimpleGuid class, my first test unit test in C++, exciting!

---
+++ src/tests/SalixEngine/core/SimpleGuid.test.cpp
@@ -0,0 +1,12 @@
+#include <doctest.h>
+#include <Salix/core/SimpleGuid.h> // The class we are testing
+
+TEST_CASE("SimpleGuid: Generation and Uniqueness")
+{
+    Salix::SimpleGuid guid1 = Salix::SimpleGuid::generate();
+    Salix::SimpleGuid guid2 = Salix::SimpleGuid::generate();
+
+    CHECK(guid1.is_valid() == true);
+    CHECK(guid2.is_valid() == true);
+    CHECK(guid1 != guid2);
+}
