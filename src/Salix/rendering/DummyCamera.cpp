eecd9f17133f6ccdd5ce55b12a774a799eec2922
Ebdsaleh <nadirsaleh@gmail.com>
2025-09-08 03:54:21 +1000

    Updated 'DummyCamera' class - Added and implemented new interface methods 'set_orthographic_size',  'get_orthographic_size', 'set_is_active', 'get_is_active', 'activate', 'deactivate'.

--- src/Salix/rendering/DummyCamera.cpp
+++ src/Salix/rendering/DummyCamera.cpp
@@ -6,9 +6,10 @@
 
 namespace Salix {
     struct DummyCamera::Pimpl{
-       glm::mat4 view_matrix = 1.0f;
-       glm::mat4 projection_matrix = 1.0f;
-
+        glm::mat4 view_matrix = 1.0f;
+        glm::mat4 projection_matrix = 1.0f;
+        float orthographic_size = 10.0f;
+        bool is_active = false;    
     };
     
     DummyCamera::DummyCamera() : pimpl(std::make_unique<Pimpl>()) {}
@@ -21,4 +22,12 @@ namespace Salix {
     const glm::mat4& DummyCamera::get_projection_matrix() {
         return pimpl->projection_matrix;
     }
+
+    float DummyCamera::get_orthographic_size() const { return pimpl->orthographic_size; }
+    void DummyCamera::set_orthographic_size(float size) { pimpl->orthographic_size = size; }
+    void DummyCamera::set_is_active(bool is_active) { pimpl->is_active = is_active; }
+    bool DummyCamera::get_is_active() {return pimpl->is_active;}
+
+    void DummyCamera::activate() { set_is_active(true); }
+    void DummyCamera::deactivate() { set_is_active(false); }
 }
--- src/Salix/rendering/DummyCamera.h
+++ src/Salix/rendering/DummyCamera.h
@@ -18,10 +18,14 @@ namespace Salix {
              static const ProjectionMode mode = ProjectionMode::Perspective;
             return mode;
         }
-        void set_orthographic_size(float size) override {(void) size;}
-
+        void set_orthographic_size(float size) override;
+        float get_orthographic_size() const;
+        void set_is_active(bool is_active) override;
+        bool get_is_active() override;
+        void activate()override;
+        void deactivate() override;
     private:
-    struct Pimpl;
-    std::unique_ptr<Pimpl> pimpl;
+        struct Pimpl;
+        std::unique_ptr<Pimpl> pimpl;
     };
 }   // namespace Salix
