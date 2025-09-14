8cf649d1b2fc5a54e5a2f651dc44f68b56a58784
Ebdsaleh <nadirsaleh@gmail.com>
2025-09-14 13:11:07 +1000

    Updated 'ITimer.h' - Added new virtual method 'calculate_sleep_duration', this is required for the 'ChronoTimer' class, and fixes the issue of not being able to maintain a consistent clock while trying to capture the 'elapsed_time_ms'.

--- src/Salix/core/ITimer.h
+++ src/Salix/core/ITimer.h
@@ -7,6 +7,7 @@
 
 #include <Salix/core/Core.h>
 #include <Salix/core/InitEnums.h>
+#include <chrono>
 
 namespace Salix { 
 
@@ -25,6 +26,12 @@ namespace Salix {
 
         // Returns the delta time calculated by the most recent tick_start().
         virtual float get_delta_time() const = 0;
+        
+        // Returns the calculated duration the timer would sleep for.
+        // We can give it a default implementation in the interface.
+        virtual std::chrono::duration<float, std::milli> calculate_sleep_duration() { 
+            return std::chrono::duration<float, std::milli>::zero(); 
+        }
     };
 
 } // namespace Salix
